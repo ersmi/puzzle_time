5805b239550d2b86e985aece2699a2fda29aa4f3
Isaac Gonzalez <ilgonzalez2013@yahoo.com>
2016-11-30 14:17:23 -0600

    models completed

--- src/pt_backend/puzzle_time/models.py
+++ src/pt_backend/puzzle_time/models.py
@@ -10,9 +10,7 @@ class Users(models.Model):
     
     Should have fields:
         prof_pic - (1)url to user's uploaded profile picture.
-        friends  - (1:*) list of users that user is friends with.
-        pictures - (1:*) list of pictures user has uploaded.
-        puzzles  - (1:*) list of puzzles the user has created.
+        friends  - (*:*) list of users that user is friends with.
     """
     prof_pic = models.ImageField(upload_to='pics')
     friends = models.ManyToManyField("self")
@@ -24,9 +22,20 @@ class Pictures(models.Model):
     Should have fields:
         link    - (1) url to picture file
         name    - (1) name specified by the user to represnt the picture
+        owner   - (*:1) user that owns the picture
         tags    - (*) list of terms to be used to group the picture.
-        puzzles - (1:*) list of puzzles that are using this picture.
+
     """
+    link = models.URLField(max_length=200)
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey('Users')
+    tags = models.CharField(max_length=200)
+
+    def settags(self,x):
+        self.tags = json.dumps(x)
+
+    def gettags(self):
+        return json.loads(self.tags)
 
 class Puzzles(models.Model):
     """
@@ -37,4 +46,9 @@ class Puzzles(models.Model):
         progress - int list/binary string/boolean array that represents
         the users progress in the puzzle. e.g. 0010010 represents a puzzle
         with 2 pieces obtained, 5 not.
+        picture  - (*:1) picture of the complete puzzle
+        owner    - (*:1) owner of the puzzle
     """
+    progress = models.CharField(max_length=200)
+    picture = models.ForeignKey('Pictures',on_delete=models.CASCADE)
+    owner = models.ForeignKey('Users')
