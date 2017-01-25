56a1c4aa2333f0dcc87d29d6ac4c5386dc67c7df
Eric Smith <er.smith@live.com>
2017-01-25 14:42:35 -0600

    Add demo, fixes for views, admin, models

--- src/pt_backend/puzzle_time/admin.py
+++ src/pt_backend/puzzle_time/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Users
+from .models import Users, Pictures, Puzzles
 
 # Register your models here.
 
 admin.site.register(Users)
+admin.site.register(Pictures)
+admin.site.register(Puzzles)
--- src/pt_backend/puzzle_time/models.py
+++ src/pt_backend/puzzle_time/models.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+import json
+
 # Create your models here.
 
 class Users(models.Model):
