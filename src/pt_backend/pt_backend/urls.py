280f8a3a506185ca5c0eb1ec91dc8a327092ba32
Eric Smith <er.smith@live.com>
2017-03-01 14:37:44 -0600

    Add views and urls conf for session debug

--- src/pt_backend/pt_backend/urls.py
+++ src/pt_backend/pt_backend/urls.py
@@ -21,6 +21,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^d/session', views.sessiondebug),
     url(r'^admin/', admin.site.urls),
     url(r'^a/', include(urls)),
 ]
--- src/pt_backend/pt_backend/views.py
+++ src/pt_backend/pt_backend/views.py
@@ -6,3 +6,13 @@ def index(request):
     General page to serve links for web facing API.
     """
     return HttpResponse("<html><b>Not Implemented</b></html>")
+
+def sessiondebug(request):
+    """
+    Form to create debug session
+    """
+    if request.method == "GET":
+        return render(request, 'debug/session.html', dict())
+    #if request.method == "POST":
+    
+    #render(request, 'debug/session.html', dict())
